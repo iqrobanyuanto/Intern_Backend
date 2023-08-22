83cecbb2f6b710bcb6bfad48ff6d7741243ebed8
iqrobanyuanto <iqrobanyuanto12345@gmail.com>
2023-08-22 20:50:13 +0700

    IPD-31 update baranginfo
    - add id ke barangModel

--- models/BarangInfo.go
+++ models/BarangInfo.go
@@ -1,14 +1,16 @@
 package models
 
 type BarangModel struct {
+	id             int
 	namaBarang     string
 	jumlahBarang   int
 	hargaBarang    int
 	kategoriBarang string
 }
 
-func NewBarang(nama, kategori string, jumlah, harga int) BarangModel {
+func NewBarang(id_ int, nama, kategori string, jumlah, harga int) BarangModel {
 	return BarangModel{
+		id:             id_,
 		namaBarang:     nama,
 		jumlahBarang:   jumlah,
 		hargaBarang:    harga,
