0fcdbb650399bf240bb4fe1d10dcae47d32b924b
iqrobanyuanto <iqrobanyuanto12345@gmail.com>
2023-08-29 12:39:27 +0700

    Update DeleteBarang
    - Merubah pengambilan id dari API berdasarkan URL parameter

--- controllers/DeleteBarang.go
+++ controllers/DeleteBarang.go
@@ -5,7 +5,6 @@ import (
 	"Intern_Backend/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"strconv"
 )
 
 // Delete godoc
@@ -19,14 +18,10 @@ import (
 func Delete(c *gin.Context) {
 	var barang models.BarangModel
 	db := config.ConnectDataBase()
-	id := map[string]int{"id": 0}
-	if err := c.ShouldBindJSON(&id); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		return
-	}
-	if db.Delete(&barang, id["id"]).RowsAffected == 0 {
+	id := c.Param("id")
+	if db.Delete(&barang, id).RowsAffected == 0 {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "id gagal dihapus"})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "id " + strconv.Itoa(id["id"]) + " deleted"})
+	c.JSON(http.StatusOK, gin.H{"message": "id " + id + " deleted"})
 }
