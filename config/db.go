4216d6b7f37964c87f9e04da7533829bce6892bc
iqrobanyuanto <iqrobanyuanto12345@gmail.com>
2023-09-01 09:32:37 +0700

    DB Config & main
    - update db config
    - update main

--- config/db.go
+++ config/db.go
@@ -2,20 +2,22 @@ package config
 
 import (
 	"Intern_Backend/models"
+	"Intern_Backend/utils"
+	"fmt"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDataBase() *gorm.DB {
-	/*
-		username := "root"
-		password := "root"        // change this to your local db
-		host := "localhost: 3306" // change this to your local db
-		database := "dbintern"    // change this to your local db
-	*/
+	username := utils.Getenv("DATABASE_USERNAME", "root")
+	password := utils.Getenv("DATABASE_PASSWORD", "password")
+	host := utils.Getenv("DATABASE_HOST", "127.0.0.1")
+	port := utils.Getenv("DATABASE_PORT", "3306")
+	database := utils.Getenv("DATABASE_NAME", "database_movie")
 
-	db, err := gorm.Open(mysql.Open("root:Antimaling2@@tcp(localhost:3306)/db_intern"), &gorm.Config{})
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err.Error())
