b46f78618422278545246efdbbf532ad8f62c000
Gapherionζ <iqrobanyuanto12345@gmail.com>
2024-07-02 03:36:59 +0700

    db config change

--- config/db.go
+++ config/db.go
@@ -11,11 +11,11 @@ import (
 )
 
 func ConnectDataBase() *gorm.DB {
-	username := utils.Getenv("DATABASE_USERNAME", "azureuser")
-	password := utils.Getenv("DATABASE_PASSWORD", "^Bronya123")
-	host := utils.Getenv("DATABASE_HOST", "mysqlserver3030.database.windows.net")
-	port, converr := strconv.Atoi(utils.Getenv("DATABASE_PORT", "1433"))
-	database := utils.Getenv("DATABASE_NAME", "industrial")
+	username := utils.Getenv("DBUSER", "root")
+	password := utils.Getenv("DBPASS", "")
+	host := utils.Getenv("DBHOST", "localhost")
+	port, converr := strconv.Atoi(utils.Getenv("DBPORT", "3306"))
+	database := utils.Getenv("DBNAME", "industrial")
 
 	if converr != nil {
 		panic(converr.Error())
